416110b09d75c73752ff702aa25de5662156d60e
Dixie5364 <kenadi.allen@gmail.com>
2025-06-11 21:30:52 -0700

    F: fix to find text of book entered

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+import sys
+from stats import get_num_words
+from stats import get_char_count
+from stats import sorted_chars
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
+
 def get_book_text(book_title):
-    with open(f"./books/{book_title}") as b:
+    with open(book_path) as b:
         book_contents = b.read()
     return book_contents
 
-from stats import get_num_words
-
-title = get_book_text("frankenstein.txt")
-
-from stats import get_char_count
-
-from stats import sorted_chars
+title = get_book_text(book_path)
 
 char_count_dict = get_char_count(title)
 sorted_list_of_chars = sorted_chars(char_count_dict)
 
 
 print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
+print(f"Analyzing book found at {book_path}")
 print("----------- Word Count ----------")
 print(f"Found {get_num_words(title)} total words")
 print("--------- Character Count -------")
